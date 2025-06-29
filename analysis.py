f4d8681ce18baa42c4673e90f62fbb49a50b583a
Josemaria Escriva Wandera <wandera.josemaria22@students.dkut.ac.ke>
2025-06-29 15:24:19 +0300

    Also the updated version

---
+++ analysis.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+from Basic_Fourier_analysis import Fourier_Analysis
+
+# Time array over two periods
+time = np.linspace(0, 2*np.pi, 1000)
+
+# Define function
+def x_of_t(t):
+    return signal.square(2* np.pi * t / 1.5, duty=2/3)
+
+# Create object
+fa = Fourier_Analysis(x_of_t, 1.5, 50)
+
+# Approximate function
+approx = fa.series(time)
+
+# Plot original vs approximation
+plt.plot(time, x_of_t(time), label='Original Signal', linestyle='--')
+plt.plot(time, approx, label='Fourier Approximation')
+plt.legend()
+plt.title("Fourier Series Approximation")
+plt.xlabel("Time (t)")
+plt.ylabel("x(t)")
+plt.grid(True)
+plt.show()
