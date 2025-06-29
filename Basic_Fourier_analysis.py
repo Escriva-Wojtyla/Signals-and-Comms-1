5a8da1198e1ee2ec6d89f5e29b300afed2e7deae
Josemaria Escriva Wandera <wandera.josemaria22@students.dkut.ac.ke>
2025-06-29 15:23:40 +0300

    This is the updated version

--- Basic_Fourier_analysis.py
+++ Basic_Fourier_analysis.py
@@ -1,29 +1,76 @@
 import numpy as np
-from scipy.integrate import quad
 import matplotlib.pyplot as plt
+from scipy.integrate import quad
 
-time = np.linspace(0, 2*np.pi, 1000)
+class Fourier_Analysis:
 
-def FST(start, stop):
-     
-      Fourier_series = 0
-      
-      for i in range(start, stop):
-        sum = ((2/np.pi*i-1) * np.sin((4/3*np.pi*i-1)*time)) + ((2/np.pi*i) * np.sin((4/3*np.pi*i)*time))
-        Fourier_series += sum
+# This constructs the object, which is a function.
+    def __init__(self, X_of_t, fundamental_period, frequency_multiple_N):
+        self.X_of_t = X_of_t
+        self.T = fundamental_period
+        self.N = frequency_multiple_N
+        self.omega = 2 * np.pi / self.T
 
-      plt.plot(Fourier_series)
-      plt.title("Generated plots")
-      plt.xlabel("Time(s)")
-      plt.ylabel("y(t)")
-      plt.grid(True)
-      plt.legend()
-      plt.show()
+#This construct the integrand functions used to compute the coeffecients, a and b
+    def construct_a_n(self, t, n):
+        return self.X_of_t(t) * np.cos(n * self.omega * t)
+
+    def construct_b_n(self, t, n):
+        return self.X_of_t(t) * np.sin(n * self.omega * t)
+    
+#Computes the coeffecients, using the quad function from the scipy.integrate lib. 
+    def coeff_a_n(self, n):
+        result, _ = quad(self.construct_a_n, 0, self.T, args=(n,))
+        return result * (2 / self.T)
+
+    def coeff_b_n(self, n):
+        result, _ = quad(self.construct_b_n, 0, self.T, args=(n,))
+        return result * (2 / self.T)
+
+    def a_0(self):
+        result, _ = quad(self.X_of_t, 0, self.T)
+        return result * (1 / self.T)
+
+#This compute the approximation, given the Number of harmonics, self.N
+    def series(self, time):
+        a0 = self.a_0()
+        s = (a0 / 2)
+        for n in range(1, self.N + 1):
+            an = self.coeff_a_n(n)
+            bn = self.coeff_b_n(n)
+            s +=  an * np.cos(n * self.omega * time) + bn * np.sin(n * self.omega * time)
+        return s
 
 
-series1 = FST(1, 2)
-series2 = FST(1, 5)
-series3 = FST(1, 10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
 
 
 
