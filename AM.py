6709a8cd7c4dd0ca84c67fcd2992c268a6cf0a13
Josemaria Escriva Wandera <wandera.josemaria22@students.dkut.ac.ke>
2025-08-10 07:58:18 +0300

    Update AM.py

--- AM.py
+++ AM.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-fs = 10000
-t = np.linespace(0, 0.01, int(fs*0.01))
+fs = 1000000
+t = np.linespace(0, 0.01, int(fs*1))
 
 a_c = 2
 a_m = 2
@@ -11,19 +11,19 @@ f_c = 10
 f_m = 1
 
 
-carrier = a_c * np.cos(20*np.pi*t)
-message = a_m * np.cos(2*np.pi*t)
+carrier = a_c * np.cos(2*np.pi*f_c*t)
+message = a_m * np.cos(2*np.pi*f_m*t)
 mod = 1 +  myu * message
-mudulated = carrier * mod
+modulated = carrier * mod
 
 plt.figure(figsize=(10,6))
-plt.plot(t, carrier, label="Cariier Signal")
+plt.plot(t, carrier, label="Carrier Signal")
 plt.plot(t, message, "---", alpha=0.4, label="Message signal")
 plt.plot(t, modulated, ":", alpha=0.5, label="AM signal")
 
 plt.xlabel("Time(s)")
 plt.ylabel("Amplitude")
-plt.title("AM signal)
+plt.title("AM signal")
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
